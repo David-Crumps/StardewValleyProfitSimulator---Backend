32e09363ac433a3467246c4f2ebb391ddf30cbb8
David-Crumps <david.crumplin@gmail.com>
2026-03-17 10:15:01 +0200

    Given a purchase of x amount of crops, this DTO holds that quantity split by quality of the crops, given a quality simulation

---
+++ src/main/java/com/davidcrumps/StardewValleyProfitSimulator/Dto/CropQualityDTO.java
@@ -0,0 +1,10 @@
+package com.davidcrumps.StardewValleyProfitSimulator.Dto;
+
+import lombok.Data;
+
+@Data
+public class CropQualityDTO {
+    private final int gold;
+    private final int silver;
+    private final int normal;
+}
