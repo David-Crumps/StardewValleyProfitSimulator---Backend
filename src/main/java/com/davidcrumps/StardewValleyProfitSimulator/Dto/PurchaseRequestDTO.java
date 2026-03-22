059647742cc6a0651f14091a1243f0ea8d435cc0
David-Crumps <david.crumplin@gmail.com>
2026-03-22 20:48:45 +0200

    Added validation on PurchaseRequestDTO to ensure incoming values are correct and exist

--- src/main/java/com/davidcrumps/StardewValleyProfitSimulator/Dto/PurchaseRequestDTO.java
+++ src/main/java/com/davidcrumps/StardewValleyProfitSimulator/Dto/PurchaseRequestDTO.java
@@ -1,11 +1,24 @@
 package com.davidcrumps.StardewValleyProfitSimulator.Dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class PurchaseRequestDTO {
-    long cropId;
-    int quantity;
-    int fertilizer;
-    int farmingLevel;
+    private long cropId;
+
+    @Min(value = 1, message = "Cannot purchase less than 1 crop")
+    private Integer quantity;
+
+    @NotNull(message = "No value found")
+    @Min(value = 0, message = "Cannot be negative")
+    @Max(value = 4, message = "Cannot exceed 4")
+    private Integer fertilizer;
+
+    @NotNull(message = "No value found")
+    @Min(value = 0, message = "Cannot be negative")
+    @Max(value = 10, message = "Cannot exceed 10")
+    private Integer farmingLevel;
 }
